dfb36ce06d93605d73d12150275c3dc5145046e8
Vincenzo Presta <vpresta9@gmail.com>
2024-09-02 12:18:53 +0200

    task 3.2/reworked project structure

---
+++ Task3/32/robotic_planning/src/fill_box_from_workstation_action_node.cpp
@@ -0,0 +1,65 @@
+#include <memory>
+#include <algorithm>
+#include<vector>
+#include<string>
+
+#include "plansys2_executor/ActionExecutorClient.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+using namespace std::chrono_literals;
+
+class FillBoxFromWorkstation : public plansys2::ActionExecutorClient
+{
+public:
+  FillBoxFromWorkstation()
+  : plansys2::ActionExecutorClient("fill-box-from-workstation", 250ms)
+  {
+    progress_ = 0.0;
+  }
+
+private:
+  void do_work()
+  { std :: vector <std :: string > arguments = get_arguments () ;
+    if (progress_ < 1.0) {
+      progress_ += 0.2;
+      send_feedback(progress_, "agent "+arguments [0]+ " is filling "+ 
+            arguments [1]+ " in "+ arguments [3] + " with "+
+            arguments [2]+ " carrying a "+
+            arguments [4]);
+    } else {
+      finish(true, progress_, "agent "+arguments [0]+ " is filling "+ 
+            arguments [1]+ " in "+ arguments [3] + " with "+
+            arguments [2]+ " carrying a "+
+            arguments [4]);
+
+      progress_ = 0.0;
+      std::cout << std::endl;
+    }
+
+    std::cout << "\r\e[K" << std::flush;
+    std::cout << "Robot "+arguments [0]+ " is filling "+ 
+            arguments [1]+ " in "+ arguments [3] + " with some "+
+            arguments [2]+ " carrying a "+
+            arguments [4]+" . . . [ " << std::min(100.0, progress_ * 100.0) << "% ]  " <<
+            std::flush;
+  }
+
+  float progress_;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<FillBoxFromWorkstation>();
+
+  node->set_parameter(rclcpp::Parameter("action_name", "fill-box-from-workstation"));
+  node->trigger_transition(lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE);
+
+  rclcpp::spin(node->get_node_base_interface());
+
+  rclcpp::shutdown();
+
+  return 0;
+}
